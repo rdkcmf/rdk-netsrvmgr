f7978fc464695bceb8c7393d495250340332b19a
Balaji Punnuru <balaji_punnuru@cable.comcast.com>
2017-07-10 18:54:25 -0400

    RDK-16702: Added support to switch paths.
    Reason for change: When gateway is detected in the network,
                       needs to make it as primary.
    
    Test Procedure: Build and Verify.
    
    Risks: Low.
    
    Change-Id: I0bbf47fec59fc7a9ac7410535ff0ddbff35a4ba0
    Signed-off-by: Balaji Punnuru <balaji_punnuru@cable.comcast.com>

--- src/services/route/src/routeSrvMgr.cpp
+++ src/services/route/src/routeSrvMgr.cpp
@@ -31,6 +31,11 @@
 #include "NetworkMedium.h"
 #include "netsrvmgrUtiles.h"
 
+#ifdef ENABLE_NLMONITOR
+#include "netlinkifc.h"
+#endif //ENABLE_NLMONITOR
+
+
 #define ROUTE_PRIORITY 50
 #define GW_SETUP_FILE "/lib/rdk/gwSetup.sh"
 #define MAX_CJSON_EMPTY_LENGTH 40
@@ -47,6 +52,8 @@ bool signalUpnpDataReady=true;
 
 RouteNetworkMgr* RouteNetworkMgr::instance = NULL;
 bool RouteNetworkMgr::instanceIsReady = false;
+
+static bool gwSelected = false;
 
 pthread_cond_t condRoute = PTHREAD_COND_INITIALIZER;
 pthread_mutex_t mutexRoute = PTHREAD_MUTEX_INITIALIZER;
@@ -70,6 +77,13 @@ int  RouteNetworkMgr::Start()
     bool retVal=false;
     IARM_Bus_RegisterEventHandler(IARM_BUS_SYSMGR_NAME,IARM_BUS_SYSMGR_EVENT_XUPNP_DATA_UPDATE,_evtHandler);
     IARM_Bus_RegisterCall(IARM_BUS_ROUTE_MGR_API_getCurrentRouteData, getCurrentRouteData);
+
+#ifdef ENABLE_NLMONITOR
+    NetLinkIfc* netifc = NetLinkIfc::get_instance();
+    netifc->initialize();
+    netifc->run(false);
+#endif
+
     RDK_LOG( RDK_LOG_TRACE1, LOG_NMGR, "[%s:%s:%d] Enter\n", MODULE_NAME,__FUNCTION__, __LINE__ );
 
     getGatewayRouteData();
@@ -221,6 +235,34 @@ void* getGatewayRouteDataThrd(void* arg)
         {
             RDK_LOG( RDK_LOG_INFO, LOG_NMGR, "[%s:%s:%d] Triggering dhcp lease since no XG gateway  \n", MODULE_NAME,__FUNCTION__, __LINE__);
             netSrvMgrUtiles::triggerDhcpLease();
+#ifdef ENABLE_NLMONITOR
+            if (gwSelected)
+            {
+               list<std::string> ifcList;
+               if (getenv("WIFI_INTERFACE") != NULL)
+               {
+                  ifcList.push_back(getenv("WIFI_INTERFACE"));
+               }
+
+               if (getenv("MOCA_INTERFACE") != NULL)
+               {
+                  ifcList.push_back(getenv("MOCA_INTERFACE"));
+               }
+               gwSelected = false;
+               for (auto const& i : ifcList)
+               {
+                  std::string ifcStr = i;
+                  std::string cmd = "/lib/rdk/enableIpv6Autoconf.sh ";
+                  cmd += ifcStr;
+                  //Invoke API to cleanup Global IPs assigned.
+                  NetLinkIfc::get_instance()->deleteinterfaceip(ifcStr,AF_INET6);
+                  NetLinkIfc::get_instance()->deleteinterfaceroutes(ifcStr,AF_INET6);
+                  //Call script to enable SLAAC ra support.
+                  system(cmd.c_str());
+                  NetLinkIfc::get_instance()->activatelink(ifcStr);
+               }
+            }
+#endif//ENABLE_NLMONITOR
         }
 //        }
 //	else
@@ -598,6 +640,37 @@ gboolean RouteNetworkMgr::setRoute() {
         }
         if(setRoute)
         {
+#ifdef ENABLE_NLMONITOR
+            if (!gwSelected)
+            {
+               list<std::string> ifcList;
+               if (getenv("WIFI_INTERFACE") != NULL)
+               {
+                  ifcList.push_back(getenv("WIFI_INTERFACE"));
+               }
+
+               if (getenv("MOCA_INTERFACE") != NULL)
+               {
+                  ifcList.push_back(getenv("MOCA_INTERFACE"));
+               }
+               gwSelected = true;
+
+                for (auto const& i : ifcList)
+                {
+                  //Call script to disable SLAAC ra support.
+                  std::string ifcStr = i;
+                  std::string cmd = "/lib/rdk/disableIpv6Autoconf.sh ";
+                  cmd += ifcStr;
+                  system(cmd.c_str());
+
+                  //Invoke API to cleanup Global IPs assigned.
+                  NetLinkIfc::get_instance()->deleteinterfaceip(ifcStr,AF_INET6);
+                  NetLinkIfc::get_instance()->deleteinterfaceroutes(ifcStr,AF_INET6);
+                  NetLinkIfc::get_instance()->activatelink(ifcStr);
+                }
+            }
+#endif //ENABLE_NLMONITOR
+
             GString *GwRouteParam=g_string_new(NULL);
             g_string_printf(GwRouteParam,"%s" ,GW_SETUP_FILE);
             g_string_append_printf(GwRouteParam," \"%s\"" ,gwdata->gwyip->str);
