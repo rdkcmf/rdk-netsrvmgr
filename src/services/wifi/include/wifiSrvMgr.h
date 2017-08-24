443bc54ec51a044be0a043b09356a721ec8a488c
Tony Ukken <Tony_Ukken2@cable.comcast.com>
2017-08-24 17:53:04 +0000

    PACEXI5-2400: Create WPA supplicant conf file from NetApp DB
    Reason for change: Create WPA supplicant conf file from NetApp DB.
    Test Procedure: Load a build that uses NetApp. Then load a build that
    uses WPA supplicant and has this change. Verify box boots up to AV.
    Risks: Low
    Signed-off-by: Tony Ukken <Tony_Ukken2@cable.comcast.com>
    
    Change-Id: Ie6af01f051904617d8f2ba6cbb14df0938fabce7

--- src/services/wifi/include/wifiSrvMgr.h
+++ src/services/wifi/include/wifiSrvMgr.h
@@ -70,6 +70,10 @@ private:
     WiFiNetworkMgr();
     virtual ~WiFiNetworkMgr();
 
+#ifndef ENABLE_XCAM_SUPPORT
+    int create_wpa_supplicant_conf_from_netapp_db (const char* wpa_supplicant_conf_file, const char* netapp_db_file);
+#endif // ENABLE_XCAM_SUPPORT
+
     static bool m_isenabled;
     static WiFiNetworkMgr* instance;
     static bool instanceIsReady;
