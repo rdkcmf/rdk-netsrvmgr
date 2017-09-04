83bdb322619567d12c052f6f6d9ef7f147a182ad
Simon Chung <simon.c.chung@accenture.com>
2017-09-04 17:17:05 +0100

    RDKCOM-102 : isWifiConnected call should be inside ENABLE_LOST_FOUND
    Reason for change: Community builds are broken with the
    ENABLE_LOST_FOUND check. LnF is not available to the Community
    Test Procedure: Builds are OK
    Risks: Low
    
    Change-Id: Ibf3cfbc7f24070dd0fdc38152b3656ddd60c8592
    Signed-off-by: Simon Chung <simon.c.chung@accenture.com>

--- src/services/wifi/src/wifiHalUtiles.cpp
+++ src/services/wifi/src/wifiHalUtiles.cpp
@@ -916,7 +916,11 @@ bool scan_Neighboring_WifiAP(char *buffer)
 
 
     RDK_LOG( RDK_LOG_TRACE1, LOG_NMGR, "[%s:%s:%d] Enter..\n", MODULE_NAME,__FUNCTION__, __LINE__ ); 
+#ifdef ENABLE_LOST_FOUND
     if((isWifiConnected()) && (RETURN_OK != wifi_disconnectEndpoint(1, wifiConnData.ssid)))
+#else
+    if(RETURN_OK != wifi_disconnectEndpoint(1, wifiConnData.ssid))
+#endif
     {
         RDK_LOG( RDK_LOG_ERROR, LOG_NMGR, "[%s:%s:%d] Failed to  Disconnect in wifi_disconnectEndpoint()",MODULE_NAME, __FUNCTION__, __LINE__);
     }
