2867e224ec27188a16d38ac59c2c138cb686b12e
Tony Ukken <Tony_Ukken2@cable.comcast.com>
2018-02-22 18:43:39 -0500

    PACEXI5-2811: WiFi password exposed in INFO level logs
    Reason for change: Avoid exposing WiFi password in INFO level logs.
    Test Procedure: Verify WiFi password is not exposed in logs that have
    log levels of INFO and above.
    Risks: None.
    Signed-off-by: Tony Ukken <Tony_Ukken2@cable.comcast.com>
    
    Change-Id: Icd0caf5a7a5a6d97dcf66d4d6802a84c86d15385

--- src/services/wifi/src/wifiHalUtiles.cpp
+++ src/services/wifi/src/wifiHalUtiles.cpp
@@ -908,7 +908,7 @@ bool connect_withSSID(int ssidIndex, char *ap_SSID, SsidSecurity ap_security_mod
         eventData.data.wifiStateChange.state = WIFI_CONNECTING;
         WiFi_IARM_Bus_BroadcastEvent(IARM_BUS_NM_SRV_MGR_NAME,  (IARM_EventId_t) eventId, (void *)&eventData, sizeof(eventData));
 #endif
-        RDK_LOG( RDK_LOG_INFO, LOG_NMGR,"[%s:%s:%d] connecting to ssid %s  with passphrase %s \n",
+        RDK_LOG( RDK_LOG_DEBUG, LOG_NMGR,"[%s:%s:%d] connecting to ssid %s with passphrase %s \n",
                  MODULE_NAME,__FUNCTION__, __LINE__, ap_SSID, ap_security_KeyPassphrase);
     }
     ret=wifi_connectEndpoint(ssidIndex, ap_SSID, securityMode, ap_security_WEPKey, ap_security_PreSharedKey, ap_security_KeyPassphrase, saveSSID,eapIdentity,carootcert,clientcert,privatekey);
