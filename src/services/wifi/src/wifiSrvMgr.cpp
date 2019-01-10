1c071d78c490044e434f7ceea93100d25418ef14
vdubey200 <vijay_dubey@cable.comcast.com>
2019-01-10 15:20:03 -0500

    RDKC-3805 : Reverting the previous change
    Reason for change: Casuing issue on RDKC
    Test Procedure: Test if L&F gets triggered on xCMA
    Risks: low
    Signed-off-by: vdubey200 <vijay_dubey@cable.comcast.com>
    
    Change-Id: I3fdfa0301e52dc0286a3a8120f4808e6d012917c

--- src/services/wifi/src/wifiSrvMgr.cpp
+++ src/services/wifi/src/wifiSrvMgr.cpp
@@ -349,9 +349,10 @@ int  WiFiNetworkMgr::Start()
     {
         if(getLAFssid())
         {
+            lafConnectToPrivate();
+            
             if(false == isWifiConnected())
             {
-                lafConnectToPrivate();
                 connectToLAF();
             }
         }
@@ -361,7 +362,7 @@ int  WiFiNetworkMgr::Start()
         }
     }
 #endif
-
+    /*Register connect and disconnect call back */
     RDK_LOG( RDK_LOG_TRACE1, LOG_NMGR, "[%s:%s:%d] Exit\n", MODULE_NAME,__FUNCTION__, __LINE__ );
 }
 
